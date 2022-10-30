40314390250b3f065932f8ac817a6cea5118984e
Marchesi Gabriele <marchesigabriele2819@gmail.com>
2022-10-30 20:43:36 +0100

    basis matmul opencl works, 4096 in 1.43

--- main_opencl.py
+++ main_opencl.py
@@ -1,16 +1,33 @@
 import blocking_algo_testing as bl
 import numpy as np
 import time
+import opencl_matmul as mm
+import sys
 
 # TODO: Sostituire le moltiplicazioni tra matrici con moltiplicazioni eseguite sulla GPU
 
-N = 5000 
-
+N = 4096 
 
 if __name__ == "__main__":
 
     # Matrice iniziale
-    P = np.random.randint(N, size=(N,N))
+    P = np.random.rand(N, N)
+    print("primo elemento: ", P[0][0], "\n")
+  
+    
+    # TEST OPENCL MATMUL 
+    #####################################################
+    start = time.monotonic()
+    np.dot(P,P) 
+    end = time.monotonic()
+    print(f"Tempo NUMPY MATMUL: {end-start}s")
+
+    start = time.monotonic()
+    m1 = mm.matmul(P,P,N)
+    end = time.monotonic()
+    print(f"Tempo OPENCL MATMUL: {end-start}s")
+    sys.exit(0)
+    #####################################################
 
     start = time.monotonic()
     inversa = bl.inversa(P)
--- opencl_matmul.py
+++ opencl_matmul.py
@@ -1,9 +1,14 @@
 import pyopencl as cl
 import numpy as np
+import os
+import warnings
+
+os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1'
+os.environ['PYOPENCL_CTX'] = '0' 
+warnings.filterwarnings("ignore")
 
 
 # https://cnugteren.github.io/tutorial/pages/page4.html
-
 
 def matmul(matrix1, matrix2, N):
     # OpenCL Setup
@@ -15,19 +20,30 @@ def matmul(matrix1, matrix2, N):
 
     # Buffers
     mf = cl.mem_flags
-    A = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_POINTER, matrix1.nbytes, hostbuf = matrix1)
-    B = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_POINTER, matrix2.nbytes, hostbuf = matrix2)
-    C = cl.Buffer(ctx, mf.WRITE_ONLY | mf.COPY_HOST_POINTER, matrix1.nbytes)
+    A = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, matrix1.nbytes, hostbuf = matrix1)
+    B = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, matrix2.nbytes, hostbuf = matrix2)
+    C = cl.Buffer(ctx, mf.WRITE_ONLY, matrix1.nbytes)
 
     # Kernels Creation
     prog = cl.Program(ctx,  """
+                            __kernel void matmul(__global float* A, __global float* B, __global float* C, int N){
+                                size_t i = get_global_id(1);
+                                size_t j = get_global_id(0);
+                                
+                                float acc = 0.0f;
+
+                                for(int c = 0; c<N; c++){
+                                    acc += A[i*N + c] * B[j + c*N]; 
+                                }
+                                C[i*N + j] = acc;
+                            }
                             """).build()
 
     # Kernel Execution
     pp = prog.matmul
 
     pp.set_args(A, B, C, np.int32(N))
-    res = cl.enqueue_nd_range_kernel(queue, pp, N)  # queue, kernel, global dims, local dims, offset
+    res = cl.enqueue_nd_range_kernel(queue, pp, [N,N], None)  # queue, kernel, global dims, local dims, offset
     queue.finish()
 
     
