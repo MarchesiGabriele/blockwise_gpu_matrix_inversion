dd981c50928786422cad8218b3972bb29e546d1f
Marchesi Gabriele <marchesigabriele2819@gmail.com>
2022-11-01 19:30:58 +0100

    refactor

--- main_opencl.py
+++ main_opencl.py
@@ -3,39 +3,28 @@ import numpy as np
 import time
 import opencl_matmul as mm
 import sys
-import test_opencl as test
 
 # TODO: Sostituire le moltiplicazioni tra matrici con moltiplicazioni eseguite sulla GPU
 
 N = 1500
 
 # TEST OPENCL MATMUL 
-def test_mat_mul(P):
-    start = time.monotonic()
-    np.dot(P,P) 
-    end = time.monotonic()
-    print(f"Tempo NUMPY MATMUL: {end-start}s")
-
-    start = time.monotonic()
-    m1 = mm.matmul(P,P,N)
-    end = time.monotonic()
-    print(f"Tempo OPENCL MATMUL: {end-start}s")
-    sys.exit(0)
-
-def test_mat_mul1():
+def test_mat_mul():
     A = np.random.rand(500, N)
     B = np.random.rand(N, 800)
-
+    
+    #numpy
     start = time.monotonic()
     m0 = np.dot(A,B) 
     end = time.monotonic()
     print(f"Tempo NUMPY MATMUL: {end-start}s")
-
+    
+    #opencl
     start = time.monotonic()
     m1 = mm.matmul(A,B,500, N, 800)
     end = time.monotonic()
     print(f"Tempo OPENCL MATMUL: {end-start}s")
-    print(np.sum(np.subtract(m1, m0)))
+    print("errore numpy vs opencl: ", np.sum(np.subtract(m1, m0)))
     sys.exit(0)
 
 
@@ -45,8 +34,7 @@ if __name__ == "__main__":
     P = np.random.rand(N, N)
     print("primo elemento: ", P[0][0], "\n")
 
-    #test_mat_mul(P)
-    test_mat_mul1()
+    test_mat_mul()
     
     start = time.monotonic()
     inversa = bl.inversa(P)
