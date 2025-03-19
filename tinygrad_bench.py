772e0bafd6a8fb7bfd3963beec3d28a228bffd31
marchesi gabriele <marchesigabriele2819@gmail.com>
2025-03-19 08:25:38 +0100

    tinygrad 500 gflops

--- tinygrad_bench.py
+++ tinygrad_bench.py
@@ -1,7 +1,7 @@
 from tinygrad.tensor import Tensor
 import time
 
-N = 8192 
+N = 4096 
 
 a,b = Tensor.rand(N,N), Tensor.rand(N,N)
 
@@ -10,7 +10,9 @@ c = (a.reshape(N,1,N) * b.T.reshape(1,N,N)).sum(axis=2)
 c.realize()
 end = time.time()
 
+
 print((c.numpy() - a.numpy() @ b.numpy()).mean())
 print("Time: ", end-start, " s")
+print("GFLOPS: ", (N**3 * 2)/((end-start)*10**9), " GFLOPS")
 
 
